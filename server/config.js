83414de6624b3d3949547bdcd73d2320fdc42f40
Grevcev.AS <grevcev.as@dns-shop.ru>
2021-10-27 20:46:10 +1000

    fix gitignore

--- server/config.js
+++
@@ -1,5 +0,0 @@
-const config = {
-  dbUrl: `mongodb+srv://grevtsevalex:${encodeURI('t9iT%jf565-wFTm')}@cluster0.fznln.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
-}
-
-module.exports = config;
