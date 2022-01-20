cb012de209ebb60a84887b2c52aa920f17aac426
Grevcev.AS <grevcev.as@dns-shop.ru>
2022-01-20 20:45:56 +1000

    menu component

--- client/src/App.vue
+++ client/src/App.vue
@@ -6,6 +6,16 @@
   <router-view />
 </template>
 
+<script>
+import Menu from "@/components/menu.vue";
+
+export default {
+  component: {
+    Menu,
+  },
+};
+</script>
+
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -13,18 +23,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
---
+++ client/src/components/menu-button.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="menu-button">
+    <img class="menu-button__icon" :src="icon"/>
+    <span class="menu-button__text">{{title}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {type: String, required: true},
+    icon: {type: String, required: true},
+  },
+};
+</script>
+
+<style scoped>
+
+.menu-button {
+  display: flex;
+  justify-content: column;
+  height: 60px;
+  width: 60px;
+}
+</style>
